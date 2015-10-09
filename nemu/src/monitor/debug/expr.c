535028b70eb3c8ab73656388f4d3d09d52d6a85e
tracer-ics2015 <tracer@njuics.org>
2015-10-09 22:47:04 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-VirtualBox 3.13.0-24-generic #47-Ubuntu SMP Fri May 2 23:31:42 UTC 2014 i686 i686 i686 GNU/Linux  22:47:04 up  3:38,  3 users,  load average: 0.46, 0.49, 0.40 905011492b48bdb7f3877a13e3d887ab53d95a23

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -197,12 +197,10 @@ static bool make_token(char *e) {
 			return false;
 		}
 	}
-	/*	
 	int k;
 	for(k = 0; k < nr_token; k++){
 		printf("%d\t%s\n", tokens[k].type, tokens[k].str);
 	}
-	*/
 	return true; 
 }
 
