736fb287f7e4d80bc17f7920bb93f1235c46cbf8
tracer-ics2015 <tracer@njuics.org>
2015-11-01 12:08:36 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-VirtualBox 3.13.0-24-generic #47-Ubuntu SMP Fri May 2 23:31:42 UTC 2014 i686 i686 i686 GNU/Linux  12:08:36 up  1:01,  3 users,  load average: 0.39, 0.31, 0.24 7d0d1bb66ca0cc9ae6f161031b2e1831694b59c

--- nemu/src/cpu/exec/my_op/push-template.h
+++ nemu/src/cpu/exec/my_op/push-template.h
@@ -3,18 +3,15 @@
 #define instr push
 
 static void do_execute() {
-	if(DATA_BYTE == 2){
-		cpu.esp -= 2;	
-		MEM_W(cpu.esp, op_src->val);
-	}
-	else if(DATA_BYTE == 4){
-		cpu.esp -= 4;
-		MEM_W(cpu.esp, op_src->val);
-	}
+	cpu.esp -= DATA_BYTE;
+	MEM_W(cpu.esp, op_src->val);
 	print_asm_template1();
 }
 
 	make_instr_helper(i);
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
 	make_instr_helper(r);
+#endif
 
 #include "cpu/exec/template-end.h"
