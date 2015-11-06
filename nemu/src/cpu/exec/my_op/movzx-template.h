69026ba515fcff43d29105f15ba878f87b18f08e
tracer-ics2015 <tracer@njuics.org>
2015-11-06 23:08:04 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-X550JX 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:08:04 up  2:00,  2 users,  load average: 0.10, 0.08, 0.12 f859289ecfaa69bae850cdc0367b9e8d5450c390

--- nemu/src/cpu/exec/my_op/movzx-template.h
+++ nemu/src/cpu/exec/my_op/movzx-template.h
@@ -4,6 +4,7 @@
 
 static void do_execute() {
 	if(DATA_BYTE == 1){
+		/*
 		if(op_dest->size == 2){
 			uint16_t temp = (uint16_t)(uint8_t)op_src->val;
 			OPERAND_W(op_dest, temp);
@@ -12,6 +13,9 @@ static void do_execute() {
 			uint32_t temp = (uint32_t)(uint8_t)op_src->val;
 			OPERAND_W(op_dest, temp);
 		}
+		*/
+		uint32_t temp = (uint32_t)(uint8_t)op_src->val;
+		OPERAND_W(op_dest, temp);
 	}	
 	else if(DATA_BYTE == 2){
 		uint32_t temp = (uint32_t)(uint16_t)op_src->val;
