afa340f5649f44c5a231a80794bd649edc5d2536
tracer-ics2015 <tracer@njuics.org>
2015-11-07 00:01:13 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-X550JX 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:01:13 up  2:54,  3 users,  load average: 0.16, 0.19, 0.14 5943c710895127dc9c9cd01dd6e3cfa424bc3840

--- nemu/src/cpu/exec/my_op/movsx-template.h
+++ nemu/src/cpu/exec/my_op/movsx-template.h
@@ -3,6 +3,26 @@
 #define instr movsx
 
 static void do_execute() {
+	uint32_t result = 0;
+
+	// printf("%x\n", ops_decoded.opcode);
+	switch (ops_decoded.opcode & 0xff){
+		case 0xbe:
+			result = (uint32_t)(int32_t)(int8_t)op_src->val;
+			// printf("0xbe %x\n", result);
+			break;
+		case 0xbf:
+			result = (uint32_t)(int32_t)(int16_t)op_src->val;
+			// printf("0xbf %x\n", result);
+			break;
+		default:
+			// printf("default %u\n", result);
+			break;
+	}
+
+	OPERAND_W(op_dest, result);
+	print_asm_template2();
+	/*
 	if(DATA_BYTE == 1){
 		if(op_dest->size == 2){
 			int16_t temp = (int16_t)(int8_t)op_src->val;
@@ -18,6 +38,7 @@ static void do_execute() {
 		OPERAND_W(op_dest, temp);
 	}
 	print_asm_template2();
+	*/
 }
 
 make_instr_helper(rm2r)
--- nemu/src/cpu/exec/my_op/movzx-template.h
+++ nemu/src/cpu/exec/my_op/movzx-template.h
@@ -3,8 +3,25 @@
 #define instr movzx
 
 static void do_execute() {
+	uint32_t result = 0;
+
+	// printf("%x\n", ops_decoded.opcode & 0xff);
+	switch (ops_decoded.opcode & 0xff){
+		case 0xb6:
+			result = op_src->val & 0x000000ff;
+			// printf("0xb6 %u\n", result);
+			break;
+		case 0xb7:
+			result = op_src->val & 0x0000ffff;
+			// printf("0xb7 %u\n", result);
+			break;
+	}
+
+	OPERAND_W(op_dest, result);
+	print_asm_template2();
+	/*
 	if(DATA_BYTE == 1){
-		/*
+		**
 		if(op_dest->size == 2){
 			uint16_t temp = (uint16_t)(uint8_t)op_src->val;
 			OPERAND_W(op_dest, temp);
@@ -13,7 +30,7 @@ static void do_execute() {
 			uint32_t temp = (uint32_t)(uint8_t)op_src->val;
 			OPERAND_W(op_dest, temp);
 		}
-		*/
+		**
 		uint32_t temp = (uint32_t)(uint8_t)op_src->val;
 		OPERAND_W(op_dest, temp);
 	}	
@@ -22,6 +39,7 @@ static void do_execute() {
 		OPERAND_W(op_dest, temp);
 	}
 	print_asm_template2();
+	*/
 }
 
 make_instr_helper(rm2r)
