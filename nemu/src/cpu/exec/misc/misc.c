a535f3a692971070ae3bc771ad8ae91e07fd6e2f
tracer-ics2015 <tracer@njuics.org>
2015-11-09 22:44:07 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-X550JX 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:44:07 up  7:46,  2 users,  load average: 0.11, 0.09, 0.11 3ba225cd5d12d7d76953a527580f165c1faed75c

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -35,7 +35,19 @@ make_helper(leave){
 make_helper(ret){
 	cpu.eip = swaddr_read(cpu.esp, 4);
 	cpu.esp += 4;
+/*
+	if(DATA_BYTE == 2){
+		cpu.eip = MEM_R(cpu.esp);
+		cpu.esp += 2;
+		cpu.eip &= 0x0000ffff;
+	}
+	else{
+		cpu.eip = MEM_R(cpu.esp);
+		cpu.esp += 4;
+	}
 
+	cpu.esp += op_src ->val;
+*/
 	//int len = decode_i_l(cpu.eip);
 	print_asm("ret");
 	//return 1 + len;
