8d42475bd28a11f5cd9bdd0b7580cafd8bd0dedb
tracer-ics2015 <tracer@njuics.org>
2015-11-10 21:17:49 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-X550JX 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:17:49 up  7:12,  2 users,  load average: 0.32, 0.22, 0.17 2fa0c716e3f65f932c15a360308102f84e1188e

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -1,23 +1,113 @@
 #include "FLOAT.h"
+#include "inttypes.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	int64_t temp_a = a;
+	int64_t temp_b = b;
+	return (temp_a * temp_b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	int s = 0;
+	s = (a < 0) ? !s : s;
+	int64_t _a = (a < 0) ? -a : a;
+	s = (b < 0) ? !s : s;
+	int64_t _b = (b < 0) ? -b : b;
+	int64_t _q = 0;
+	_a <<= 32;
+	_b <<= 32;
+	int shl = 0, shr = 0;
+	while ((_b > _a) && ((_b >> 1) <= _a)){
+		_b >>= 1;
+		++shr;
+	}
+	while ((_b < _a) && ((_b << 1) <= _a)){
+		_b <<= 1;
+		++shl;
+	}
+
+	return ((a << 12) / (b >> 2)) << 2;
+	while (_b){
+		if (_a >= _b){
+			_a = _a - _b;
+			_q = (_q << 1) + 1;
+			_b >>= 1;
+		} else{
+			_q = _q << 1;
+			_b >>= 1;
+		}
+	}
+	_q = (_q << shl) >> (shr);
+	_q = s ? -_q : _q;
+
+	return _q;
 }
 
 FLOAT f2F(float a) {
+
+	int f2int_a = *(int *)(&a); 
+	//int float_S = f2int_a & 0x80000000;
+	int len_M = 23;
+	int float_S = ((unsigned int)f2int_a) >> 31;
+	int float_E = (f2int_a >> len_M) & 0xff;
+	int float_M = f2int_a & 0x007fffff;
+	int temp_off = 0;
+
+	if (float_E == 255)
+		return 0x7fffffff;
+	else if (float_E == 0){
+		if(float_M == 0)
+			return 0;
+		else{
+			while (len_M && !(float_M & 0x1)){
+				float_M >>= 1;
+				len_M--;
+			}
+
+			float_E -= 0x7e;
+			temp_off = len_M - float_E - 16;
+
+			if(temp >= 0)
+				float_M >>= temp_off;
+			else
+				float_M <<= -temp_off;
+
+			if(float_S == 1)
+				float_M = -float_M;
+
+			return float_M;
+		}
+	}
+	else if (float_E >= 1 && float_E <= 254)
+	{
+		while (len_M && !(float_M & 0x1)){
+			float_M >>= 1;
+			len_M--;
+		}
+
+		float_M |= 1 << len_M;
+		float_E -= 0x7f;
+		temp_off = 16 - len_M + float_E;
+
+		if (temp_off >= 0)
+			m <<= temp_off;
+		else
+			m >>= -temp_off;
+
+		if(float_S == 1)
+			float_M = -float_M;
+
+		return float_M;
+	}
+
 	nemu_assert(0);
 	return 0;
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	return (a < 0) ? -a : a;
 }
 
 FLOAT sqrt(FLOAT x) {
