e0dbb70c53ee2ef8470d1adabd97b724ca1edf55
hzw <1359434736@qq.com>
2015-11-12 22:35:34 +0800

    20151112-PA2.2-4

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -19,7 +19,7 @@ uint32_t get_ucr3();
 uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
-
+	int i;
 	uint8_t buf[4096];
 
 #ifdef HAS_DEVICE
@@ -31,24 +31,26 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	//panic("please implement me");
+	for(i = 0; i < elf->e_phnum; i++) {
+		ph = (void *)(buf + elf->e_phoff);
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
+			 ramdisk_read((uint8_t *)(ph->p_vaddr), ph->p_offset, ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
+			 memset((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 
 #ifdef IA32_PAGE
