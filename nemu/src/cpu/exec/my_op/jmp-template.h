623d64e07c9aaa7a0d7a4a939458f2b3fdf8ae7d
tracer-ics2015 <tracer@njuics.org>
2015-11-14 19:36:36 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-X550JX 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:36:36 up  6:29,  3 users,  load average: 0.23, 0.33, 0.22 6851d2d29141750d49729bbc2699d34247f5e20e

--- nemu/src/cpu/exec/my_op/jmp-template.h
+++ nemu/src/cpu/exec/my_op/jmp-template.h
@@ -30,12 +30,14 @@ static void do_execute() {
 			if (DATA_BYTE == 2)
 				cpu.eip &= 0x0000ffff;
 			break;
+		/*
 		case 0xff:
 			cpu.eip = op_src->val;
 			if (DATA_BYTE == 2)
 				cpu.eip &= 0x0000ffff;
 			cpu.eip -= 2;
 			break;
+		*/
 	}
 	print_asm_template1();
 }
@@ -43,7 +45,17 @@ static void do_execute() {
 	make_instr_helper(i)
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-	make_instr_helper(rm)
+	//make_instr_helper(rm)
+	make_helper(concat(jmp_rm_, SUFFIX)){
+	concat(decode_rm_, SUFFIX)(eip + 1);
+	if(2 == DATA_BYTE){
+		cpu.eip = ( op_src->val) & 0x0000ffff;
+	}
+	else if(4 == DATA_BYTE){
+		cpu.eip =  op_src->val;
+	}
+	return 0;	
+}
 #endif
 
 #include "cpu/exec/template-end.h"
