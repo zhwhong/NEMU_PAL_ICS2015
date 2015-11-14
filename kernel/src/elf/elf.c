ffabf7bfe7c95541da3cf2a9ef651fb71c93a40c
tracer-ics2015 <tracer@njuics.org>
2015-11-14 20:43:06 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-X550JX 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:43:06 up  7:36,  2 users,  load average: 0.04, 0.09, 0.19 82f681cbcb0e0c78eb0ff476613f223f8408fd16

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -55,7 +55,6 @@ uint32_t loader() {
 			 */
 			 memset((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
-
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t brk;
