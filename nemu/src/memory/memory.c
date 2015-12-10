7dfc2bb80b206d8bd1994c7b3678b8f4ec9938ab
hzw <1359434736@qq.com>
2015-12-10 21:51:02 +0800

    20151210-PA3_Cache_begin

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "burst.h"
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
@@ -6,10 +7,36 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	uint32_t offset = addr & BURST_MASK;
+	uint8_t result[len];
+	dram_addr temp;
+	temp.addr = addr & ~BURST_MASK;
+	uint32_t rank = temp.rank;
+	uint32_t bank = temp.bank;
+	uint32_t row = temp.row;
+	uint32_t col = temp.col;
+	if(rowbufs[rank][bank].valid && rowbufs[rank][bank].row_idx == row) {
+		if(offset + len <= BURST_LEN){
+			return unalign_rw(rowbufs[rank][bank].buf + col + offset, len);
+		}
+		else{
+			memcpy(result, rowbufs[rank][bank].buf + col + offset, BURST_LEN - offset);
+			temp.addr = (addr + BURST_LEN) & ~BURST_MASK;
+			rank = temp.rank;
+			bank = temp.bank;
+			row = temp.row;
+			col = temp.col;
+			memcpy(result + BURST_LEN - offset, rowbufs[rank][bank].buf + col, len + offset - BURST_LEN);
+			return unalign_rw(result, len);
+		}
+	}
+	else{
+		return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	}
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
+
 	dram_write(addr, len, data);
 }
 
