98b03db916787951ca1d7e4b8ac37c735ae308a4
tracer-ics2015 <tracer@njuics.org>
2016-01-06 11:48:22 +0800

    >  compile NEMU 141220000 shsf-hzw Linux shsfhzw-X550JX 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  11:48:22 up  1:59,  2 users,  load average: 0.28, 0.24, 0.28 2be561c3f2eda2a5d96e0bfeef4d40d94d0dda3d

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -197,7 +197,7 @@ uint32_t L2cache_read(hwaddr_t addr,  size_t len) {
 void L2cache_write(hwaddr_t addr, size_t len, uint32_t data) {
 	int i;
 	L2cache_addr caddr;
-	//L2cache_addr dram_addr;
+	L2cache_addr dram_addr;
 	caddr.addr = addr;
 	for(i = 0; i < Q_WIDTH2; i++) {
 		if (L2cache[caddr.r][i].q == caddr.q && L2cache[caddr.r][i].f == caddr.f && L2cache[caddr.r][i].valid == 1)  {
@@ -206,30 +206,32 @@ void L2cache_write(hwaddr_t addr, size_t len, uint32_t data) {
 			return ;
 		}
 	}
-	dram_write(addr, len, data);
-	// for (i=0;i<Q_WIDTH2;i++) {
-	// 	if (L2cache[caddr.r][i].valid == 0) {
-	// 		L2cache[caddr.r][i].q = caddr.q;
-	// 		L2cache[caddr.r][i].f = caddr.f;
-	// 		L2cache[caddr.r][i].valid = 1;
-	// 		L2cache[caddr.r][i].dirty = 0;
-	// 		update_cache(addr, L2cache[caddr.r][i].block, BLOCK_SIZE2);
-	// 		return ;
-	// 	} 
-	// }
-	// srand(time(0));
-	// i = rand()%BLOCK_NUM2;
-	// if (L2cache[caddr.r][i].dirty == 1) {
-	// 	dram_addr.q = L2cache[caddr.r][i].q;
-	// 	dram_addr.r = caddr.r;
-	// 	dram_addr.f = L2cache[caddr.r][i].f;
-	// 	dram_addr.w = 0;
-	// 	update_dram(dram_addr.addr, L2cache[caddr.r][i].block, BLOCK_SIZE2);
-	// }
-	// L2cache[caddr.r][i].q = caddr.q;
-	// L2cache[caddr.r][i].f = caddr.f;
-	// L2cache[caddr.r][i].valid = 1;
-	// L2cache[caddr.r][i].dirty = 0;
-	// update_cache(addr, L2cache[caddr.r][i].block, BLOCK_SIZE2);
-	// return ;
+	//dram_write(addr, len, data);
+	 for(i = 0; i < Q_WIDTH2; i++) {
+	 	if (L2cache[caddr.r][i].valid == 0) {
+	 		L2cache[caddr.r][i].q = caddr.q;
+	 		L2cache[caddr.r][i].f = caddr.f;
+	 		L2cache[caddr.r][i].valid = 1;
+	 		update_cache(addr, L2cache[caddr.r][i].block, BLOCK_SIZE2);
+	 		memcpy(&L2cache[caddr.r][i].block[caddr.w], &data, len);
+	 		L2cache[caddr.r][i].dirty = 1;
+	 		return ;
+	 	} 
+	 }
+	 srand(time(0));
+	 i = rand()%BLOCK_NUM2;
+	 if (L2cache[caddr.r][i].dirty == 1) {
+	 	dram_addr.q = L2cache[caddr.r][i].q;
+	 	dram_addr.r = caddr.r;
+	 	dram_addr.f = L2cache[caddr.r][i].f;
+		dram_addr.w = 0;
+	 	update_dram(dram_addr.addr, L2cache[caddr.r][i].block, BLOCK_SIZE2);
+	 }
+	 L2cache[caddr.r][i].q = caddr.q;
+	 L2cache[caddr.r][i].f = caddr.f;
+	 L2cache[caddr.r][i].valid = 1;
+	 memcpy(&L2cache[caddr.r][i].block[caddr.w], &data, len);
+	 L2cache[caddr.r][i].dirty = 1;
+	 //update_cache(addr, L2cache[caddr.r][i].block, BLOCK_SIZE2);
+	 return ;
 }
